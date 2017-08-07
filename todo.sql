3cc9a845eb975db652ea7d9a4804a64fe0b97316
Emily Deans <emilyldeans@gmail.com>
2017-08-07 16:03:49 -0400

    Corrected 'INSERT' statement errors.

--- todo.sql
+++ todo.sql
@@ -7,12 +7,19 @@ CREATE TABLE todos (
   completed_at TIMESTAMP NULL
 );
 
-INSERT INTO todolistdb (title, details, priority, created_at, completed_at) VALUES todos ('Finish my homework!', 'Complete the daily project and then work on previous submissions.', 1, now(), now());
-INSERT INTO todolistdb (title, details, priority, created_at, completed_at) VALUES todos ('Finish World War Z', 'Finish the book!', 2, now(), );
-INSERT INTO todolistdb (title, details, priority, created_at, completed_at) VALUES todos ('Fold clothes', 'Fold and organize clean clothes', 5, now(), );
-INSERT INTO todolistdb (title, details, priority, created_at, completed_at) VALUES todos ('Concert Tickets', 'Purchase tickets for upcoming shows', 3, now(), );
-INSERT INTO todolistdb (title, details, priority, created_at, completed_at) VALUES todos ('Blog', 'Finish Monday blog entry', 4, now(), );
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Finish my homework!', 'Complete the daily project and then work on previous submissions.', 1, now(), now());
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Finish World War Z', 'Finish the book!', 2, now(), null);
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Fold clothes', 'Fold and organize clean clothes', 5, now(), null);
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Concert Tickets', 'Purchase tickets for upcoming shows', 3, now(), null);
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Blog', 'Finish Monday blog entry', 4, now(), null);
 
 SELECT * FROM todos WHERE completed_at IS NULL;
 
 SELECT * FROM todos WHERE priority > 1;
+
+UPDATE todos SET 
+
+
+
+Write an UPDATE statement to complete one todo by its id. Your ids may differ, so you will choose the id to up.
+Write a DELETE statement to delete all completed todos.
