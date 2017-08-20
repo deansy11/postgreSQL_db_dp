a5fcde1981407593361607e338d3d558450ba086
Emily Deans <emilyldeans@gmail.com>
2017-08-20 11:56:30 -0400

    Created final queries.

---
+++ todo2.sql
@@ -0,0 +1,34 @@
+INSERT INTO todos (title, details, priority, created_at, completed_at)
+  VALUES ('Finish Hunger by Roxanne Gay', 'Finish the book!', 2, now(), null);
+  VALUES ('Finish Ego is the Enemy', 'Finish the book!', 2, now(), now());
+  VALUES ('Complete Gabble', 'Fold and organize clean clothes', 5, now(), null);
+  VALUES ('Concert Tickets for Joseph', 'Purchase tickets for upcoming shows', 3, now(), null);
+  VALUES ('Blog', 'Finish Monday blog entry', 4, now(), now());
+  VALUES ('Finish daily project!', 'Complete the daily project and then work on previous submissions.', 1, now(), now());
+  VALUES ('Finish robots', 'Finish the book!', 2, now(), null);
+  VALUES ('Fold clothes', 'Fold and organize clean clothes', 5, now(), null);
+  VALUES ('Concert Tickets for Cher', 'Purchase tickets for upcoming shows', 3, now(), null);
+  VALUES ('Listen to Carly Rae Jepson', 'Finish Monday blog entry', 3, now(), now());
+  VALUES ('Finish my homework!', 'Complete the daily project and then work on previous submissions.', 1, now(), now());
+  VALUES ('Laundry', 'Finish the book!', 1, now(), null);
+  VALUES ('Fold clothes again', 'Fold and organize clean clothes', 1, now(), null);
+  VALUES ('Concert Tickets', 'Purchase tickets for upcoming shows', 3, now(), null);
+  VALUES ('Blog', 'Finish Monday blog entry', 4, now(), now());
+  VALUES ('Write a thank you card!', 'Complete the daily project and then work on previous submissions.', 1, now(), now());
+  VALUES ('Watch Atomic Blonde', 'Finish the book!', 1, now(), null);
+  VALUES ('Cook dinner', 'Fold and organize clean clothes', 5, now(), null);
+  VALUES ('Concert Tickets for Gordi', 'Purchase tickets for upcoming shows', 3, now(), null);
+  VALUES ('Blog again and again', 'Finish Monday blog entry', 4, now(), null);
+
+SELECT * FROM todos WHERE priority = 3;
+
+SELECT COUNT(completed_at IS NULL) FROM todos WHERE priority = 3;
+
+SELECT COUNT(*)
+FROM todos
+WHERE priority = 1 AND created_at > NOW()::date - 30;
+
+SELECT title, details, priority, created_at FROM todos
+WHERE priority = 1
+AND created_at =
+  (SELECT MAX(created_at)
