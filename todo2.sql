a328bf0627c6d3e00e0613a6d559e6b18bdcd288
Emily Deans <emilyldeans@gmail.com>
2017-08-21 11:33:09 -0400

    Refactored with new created and completed at inputs.

--- todo2.sql
+++ todo2.sql
@@ -1,10 +1,10 @@
 INSERT INTO todos (title, details, priority, created_at, completed_at)
   VALUES ('Finish Hunger by Roxanne Gay', 'Finish the book!', 2, now(), null);
-  VALUES ('Finish Ego is the Enemy', 'Finish the book!', 2, now(), now());
+  VALUES ('Finish Ego is the Enemy', 'Finish the book!', 2, '2017-08-18 9:30', now());
   VALUES ('Complete Gabble', 'Fold and organize clean clothes', 5, now(), null);
   VALUES ('Concert Tickets for Joseph', 'Purchase tickets for upcoming shows', 3, now(), null);
-  VALUES ('Blog', 'Finish Monday blog entry', 4, now(), now());
-  VALUES ('Finish daily project!', 'Complete the daily project and then work on previous submissions.', 1, now(), now());
+  VALUES ('Blog', 'Finish Monday blog entry', 4, '2017-11-12 9:50', now());
+  VALUES ('Finish daily project!', 'Complete the daily project and then work on previous submissions.', 1, now(), '2017-08-18 9:30';
   VALUES ('Finish robots', 'Finish the book!', 2, now(), null);
   VALUES ('Fold clothes', 'Fold and organize clean clothes', 5, now(), null);
   VALUES ('Concert Tickets for Cher', 'Purchase tickets for upcoming shows', 3, now(), null);
@@ -13,7 +13,7 @@ INSERT INTO todos (title, details, priority, created_at, completed_at)
   VALUES ('Laundry', 'Finish the book!', 1, now(), null);
   VALUES ('Fold clothes again', 'Fold and organize clean clothes', 1, now(), null);
   VALUES ('Concert Tickets', 'Purchase tickets for upcoming shows', 3, now(), null);
-  VALUES ('Blog', 'Finish Monday blog entry', 4, now(), now());
+  VALUES ('Blog', 'Finish Monday blog entry', 4, '2017-08-18 9:30', now());
   VALUES ('Write a thank you card!', 'Complete the daily project and then work on previous submissions.', 1, now(), now());
   VALUES ('Watch Atomic Blonde', 'Finish the book!', 1, now(), null);
   VALUES ('Cook dinner', 'Fold and organize clean clothes', 5, now(), null);
@@ -22,7 +22,7 @@ INSERT INTO todos (title, details, priority, created_at, completed_at)
 
 SELECT * FROM todos WHERE priority = 3;
 
-SELECT COUNT(completed_at IS NULL) FROM todos WHERE priority = 3;
+SELECT COUNT(completed_at IS NULL) FROM todos GROUP BY priority;
 
 SELECT COUNT(*)
 FROM todos
